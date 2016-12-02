93884c0d1d1adc3d51b8f37bc2ed590471942e67
Carlos Cessa <carlos@pixative.com>
2016-12-02 08:59:12 -0600

    Add BETA Label
    - Added `BETA` label in the anticorruption pages title

--- themes/proparty/templates/parts/anticorruption-top.php
+++ themes/proparty/templates/parts/anticorruption-top.php
@@ -1,6 +1,6 @@
 <div class="resources-results-header">
     <div class="content_wrap">
-        <h1 class="hidden-xs">Anticorruption Open Data Package</h1>
+        <h1 class="hidden-xs">Anticorruption Open Data Package<span>beta</span></h1>
         <h4 class="page_title">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</h4>
     </div>
 </div>
