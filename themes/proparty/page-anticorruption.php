6e7b609d5fe908147e473d66a5be23b0071dce7c
Carlos Cessa <carlos@pixative.com>
2016-12-06 14:22:44 -0600

    Updated Menu Styling

--- themes/proparty/page-anticorruption.php
+++ themes/proparty/page-anticorruption.php
@@ -2,19 +2,21 @@
 <section id="anticorruption">
 <section class="[ columns_wrap sc_columns sc_columns_count_12 columns_fluid ][ anticorruption ]">
     <div class="[ column-3_12 sc_column_item ][ anticorruption-sidebar ]">
-        <ul>
-            <li class="active"><a href="<?php echo get_bloginfo( 'url' ); ?>/anticorruption">Home</a></li>
-            <li><a href="<?php echo get_bloginfo( 'url' ); ?>/analytical-framework">Section 2: Analytical Framework</a></li>
-            <li><a href="#">Section 2: Anti-Corruption Open Data</a></li>
-            <li><a href="#">Section 3: Anti-Corruption Open Data Cases</a></li>
-            <li><a href="#">Anex 1: Anti-Corruption Datasets in Detail</a></li>
-        </ul>
-        <div class="anticorruption-sponsors">
-            <p class="sponsor-label">Sponsored By</p>
-            <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_ocp.png" alt="" />
-            <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_transparencia.png" alt="" />
-            <p class="sponsor-label">Collaborative product of:</p>
-            <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_open_data.png" alt="" />
+        <div class="sidebar-container">
+            <ul>
+                <li class="active"><a href="<?php echo get_bloginfo( 'url' ); ?>/anticorruption">Home</a></li>
+                <li><a href="<?php echo get_bloginfo( 'url' ); ?>/analytical-framework">Section 2: Analytical Framework</a></li>
+                <li><a href="#">Section 2: Anti-Corruption Open Data</a></li>
+                <li><a href="#">Section 3: Anti-Corruption Open Data Cases</a></li>
+                <li><a href="#">Anex 1: Anti-Corruption Datasets in Detail</a></li>
+            </ul>
+            <div class="anticorruption-sponsors">
+                <p class="sponsor-label">Sponsored By</p>
+                <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_ocp.png" alt="" />
+                <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_transparencia.png" alt="" />
+                <p class="sponsor-label">Collaborative product of:</p>
+                <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_open_data.png" alt="" />
+            </div>
         </div>
     </div>
     <div class="[ column-9_12 sc_column_item ][ anticorruption-content ]">
--- themes/proparty/page-challenge-corruption.php
+++ themes/proparty/page-challenge-corruption.php
@@ -2,25 +2,27 @@
 <section id="anticorruption">
 <section class="[ columns_wrap sc_columns sc_columns_count_12 columns_fluid ][ anticorruption ]">
     <div class="[ column-3_12 sc_column_item ][ anticorruption-sidebar ]">
-        <ul>
-            <li><a href="/anticorruption">Home</a></li>
-            <li class="active">
-                <a href="<?php echo get_bloginfo( 'url' ); ?>/analytical-framework">Section 2: Analytical Framework</a>
-                <ul>
-                    <li class="active"><a href="<?php echo get_bloginfo( 'url' ); ?>/challenge-corruption">Metting the challenge of corruption</a></li>
-                    <li><a href="<?php echo get_bloginfo( 'url' ); ?>/analytical-framework">Linking open data to anti-corruption strategies</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Section 2: Anti-Corruption Open Data</a></li>
-            <li><a href="#">Section 3: Anti-Corruption Open Data Cases</a></li>
-            <li><a href="#">Anex 1: Anti-Corruption Datasets in Detail</a></li>
-        </ul>
-        <div class="anticorruption-sponsors">
-            <p class="sponsor-label">Sponsored By</p>
-            <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_ocp.png" alt="" />
-            <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_transparencia.png" alt="" />
-            <p class="sponsor-label">Collaborative product of:</p>
-            <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_open_data.png" alt="" />
+        <div class="sidebar-container">
+            <ul>
+                <li><a href="<?php echo get_bloginfo( 'url' ); ?>/anticorruption">Home</a></li>
+                <li class="active submenu">
+                    <a href="<?php echo get_bloginfo( 'url' ); ?>/analytical-framework">Section 2: Analytical Framework</a>
+                    <ul>
+                        <li class="active"><a href="<?php echo get_bloginfo( 'url' ); ?>/challenge-corruption">Metting the challenge of corruption</a></li>
+                        <li><a href="<?php echo get_bloginfo( 'url' ); ?>/analytical-framework">Linking open data to anti-corruption strategies</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Section 2: Anti-Corruption Open Data</a></li>
+                <li><a href="#">Section 3: Anti-Corruption Open Data Cases</a></li>
+                <li><a href="#">Anex 1: Anti-Corruption Datasets in Detail</a></li>
+            </ul>
+            <div class="anticorruption-sponsors">
+                <p class="sponsor-label">Sponsored By</p>
+                <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_ocp.png" alt="" />
+                <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_transparencia.png" alt="" />
+                <p class="sponsor-label">Collaborative product of:</p>
+                <img src="<?php bloginfo('template_directory'); ?>/images/logos/logo_open_data.png" alt="" />
+            </div>
         </div>
     </div>
     <div class="[ column-9_12 sc_column_item ][ anticorruption-content ]">
