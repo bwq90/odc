77f5c1fd5ed14af515cc9525e0a85b3a6673f4c1
carlos maya <beto@carlosmaya.mx>
2016-12-07 23:28:40 -0600

    removes copy in header

--- themes/proparty/templates/parts/open-data-package-top.php
+++ themes/proparty/templates/parts/open-data-package-top.php
@@ -1,6 +1,5 @@
 <div class="resources-results-header odp-header">
     <div class="content_wrap">
         <h1>Agriculture Open Data Package<span>beta</span></h1>
-        <h4 class="page_title"><em>A roadmap for governments to publish data as open data to empowering farmers, optimising agricultural practice, stimulating rural finance, facilitating the agri value chain, enforcing policies, and promoting government transparency and efficiency.</em></h4>
     </div>
 </div>
